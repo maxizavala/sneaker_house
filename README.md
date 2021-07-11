def8688a40517e709e486428b86d124f407fa6e5
Maximiliano Zavala <maximilianozavala@MacBook-Pro-de-Maximiliano-2.local>
2021-07-11 14:39:31 -0300

    Eliminacion archivos Json y funciones asociadas

--- README.md
+++ README.md
@@ -1,12 +1,58 @@
 # sneaker_house
 Da Vinci -  Produccion Web => php + bootstrap + ajax 
 
-## UNCHECK
-### MAXI
-01 - Pagina Admin 
-    . edit Categorias.   
-02 - producto.php. 
-    . Producto nuevo.  
-        . Subir imagen. 
-    . Eliminar producto. 
+### INDEX.PHP. 
+    .Muestra carrousel. 
+    .Muestra categorias que filtran productos (se modifican desde admin). Se conecta a la BD. 
+    .Muestra productos: Muestra el listado de productos. Se conecta a la BD. 
+    .Agrega/Eliminar productos a favoritos (Se conecta a la BD)
+
+### PRODUCTO.PHP. 
+    .Muestra el detalle del producto. Se conecta a la BD. 
+    .Agrega productos al carrito (Se conecta a la BD). 
+    .Agrega/Elimina productos a favoritos (Se conecta a la BD)
+
+### EDIT_PRODUCTO.PHP. 
+    .ABM de productos. Se conecta a la BD. 
+    .Sube imagenes al servidor. 
+
+### CONTACTO.PHP. 
+    .Muestra detalle de contacto (los datos los toma desde la BD). 
+    .Formulario de contacto. (envia los contactos generados por email al admnistrador). 
+
+### REGISTRO.PHP. 
+    .Crea nuevos usuarios (Se conecta a la BD). 
+
+### ADMIN.PHP. 
+    .Modifica datos del header, footer y email del formulario de contacto. 
+    .ABM de Categorias. 
+
+### CARRITO.PHP. 
+    .Muestra productos en carrito (se conecta a la BD). 
+    .Confirma/Cancela la compra (Se conecta a la BD). 
+
+### HEADER.PHP. (script unico). 
+    .Secciones del sitio. 
+    .Subtitulo modificable (se conecta a la BD). 
+    .login/logout (se conecta a la BD). 
+    .Muestra favoritos (Se conecta a la BD). 
+
+### FOOTER.PHP. (script unico). 
+    .links a redes sociales que se modifican desde admin (Se conecta a BD). 
+
+### USUARIOS
+ADMIN. 
+usuario: zeta. 
+pass: dv01. 
+    - ABM de productos. 
+    - ABM categorias. 
+    - Pueden modificar aspectos del sitio.
+    - Pueden agregar productos a favoritos. 
+    - Pueden comprar. 
+
+COMUN. 
+usuario: pepe. 
+pass: dv01. 
+    - Pueden agregar productos a favoritos. 
+    - Pueden comprar.
 
--- inc/funciones.php
+++ inc/funciones.php
@@ -1,58 +1,5 @@
 <?php
 
-//Crea un archivo Json a partir de un array en una carpeta especificada.
-    function GuardarArrayEnJson($carpeta, $nombre_archivo, $array){ 
-        if(!file_exists($carpeta)){ //Si el directorio NO existe...
-            mkdir($carpeta); //crea el directorio
-        }
-        $fp = fopen($carpeta."/".$nombre_archivo, 'w');
-        fwrite($fp, json_encode($array));
-        fclose($fp);
-    }
-
-    //Lee un archivo Json y devuelve un array
-	function LeerArrayJson($carpeta, $nombre_archivo){ 
-        $fp = fopen($carpeta."/".$nombre_archivo, 'r');
-        $json = fread($fp, filesize($carpeta."/".$nombre_archivo));
-        fclose($fp);
-        $array = json_decode($json, true);
-        return $array;
-    }
-    
-    //Muestra el contenido de cada comentario
-    function MostrarComentarios($carpeta, $nombre_json, $id_producto){
-        $array = LeerArrayJson($carpeta, $nombre_json); //obtengo array del json
-        $ca = count($array); //ultimo comentario -> donde inicio el recorrido
-        $count = 1; //contador de comentarios
-        for ($i=$ca; $i >= 1; $i--) { 
-            if ($array[$i]['id_producto'] == $id_producto && $count<4) { // si el id del array == id parametro && contador de comentarios menor a 4 (porq se muestran losultimo 3)
-                echo "<p>".$array[$i]['usuario']." | ".$array[$i]['fecha']."<p>";
-                echo "<p>".$array[$i]['mensaje']."<p>";
-                echo "<p>Valoracion: ".$array[$i]['valoracion']."<p>";
-                echo "<hr>";
-                $count++;
-            }
-        }
-    }
-
-    //Obtiene el array del producto
-    function MostrarProducto($carpeta, $nombre_json, $id_producto){
-        $array = LeerArrayJson($carpeta, $nombre_json);
-        for ($i=1; $i <= count($array); $i++) { 
-            if ($array[$i]['id_producto'] == $id_producto) {
-                $a_producto = $array[$i]; 
-            }
-        }
-        return $a_producto;
-    }
-
-    //Mostrar categoria (String) del producto
-    function MostrarCategoria($carpeta, $nombre_json, $id_categoria){
-        $array = LeerArrayJson($carpeta, $nombre_json);
-        $categoria = $array[$id_categoria]['nombre'];
-        return $categoria;
-    }
-
     //Etiqueta active del boostrap segun pagina donde se esta
     function MenuDinamico($pagina){
         if (strpos($_SERVER['PHP_SELF'], $pagina)) {
@@ -60,24 +7,12 @@
         }
     }
 
-    //Etiqueta title segun producto
-    function MostrarTitulo($id_producto){
-        $array = LeerArrayJson('json', 'productos.json');
-        for ($i=1; $i <= count($array); $i++) { 
-            if ($array[$i]['id_producto'] == $id_producto) {
-                $a_producto = $array[$i]; 
-            }
-        }
-        return $a_producto['marca']." ".$a_producto['modelo'];
-    }
+
 
     //Etiqueta title segun pagina donde se esta
     function EtiquetaTitle(){
         if (strpos($_SERVER['PHP_SELF'], 'index.php')) {
             echo "Home";
-        }
-        elseif (strpos($_SERVER['PHP_SELF'], 'producto.php')) {
-            echo MostrarTitulo($_REQUEST['id']);
         }
         elseif (strpos($_SERVER['PHP_SELF'], 'contacto.php')) {
             echo "Contacto";
